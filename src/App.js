cf445c65c2fdb4c0f5248a7c6bb88fcf12ac4b52
sameeramp <sameeramp2000@gmail.com>
2023-12-03 01:45:22 +0530

    assignment using next js completed successfully

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './Components/Login';
+import './styles/tailwind.css';
+
+function App() {
+  return (
+    <div className="App">
+  <Login/>
+    </div>
+  );
+}
+
+export default App;
